9e98e2eca5e8790bb928d2e0d960529bc986f754
y4shiro <y4shironao@gmail.com>
2021-05-08 21:20:19 +0900

    20. Tweet投稿フォーム

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -5,7 +5,7 @@ import { selectUser } from '../features/userSlice';
 import { auth, storage, db } from '../firebase';
 
 import { Avatar, Button, IconButton } from '@material-ui/core';
-import AddPhotoIcon from '@material-ui/icons/AddAPhoto';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import firebase from 'firebase/app';
 
 const TweetInput = () => {
@@ -69,15 +69,50 @@ const TweetInput = () => {
   };
 
   return (
-    <div>
-      <Avatar
-        className={styles.tweet_avatar}
-        src={user.photoUrl}
-        onClick={async () => {
-          await auth.signOut();
-        }}
-      />
-    </div>
+    <>
+      <form onSubmit={sendTweet}>
+        <div className={styles.tweet_form}>
+          <Avatar
+            className={styles.tweet_avatar}
+            src={user.photoUrl}
+            onClick={async () => {
+              await auth.signOut();
+            }}
+          />
+          <input
+            className={styles.tweet_input}
+            placeholder="What's happening?"
+            type="text"
+            autoFocus
+            value={tweetMsg}
+            onChange={(e) => setTweetMsg(e.target.value)}
+          />
+          <IconButton>
+            <label>
+              <AddAPhotoIcon
+                className={
+                  tweetImage ? styles.tweet_addIconLoaded : styles.tweet_addIcon
+                }
+              />
+              <input
+                className={styles.tweet_hiddenIcon}
+                type="file"
+                onChange={onChangeImageHandler}
+              />
+            </label>
+          </IconButton>
+        </div>
+        <Button
+          type="submit"
+          disabled={!tweetMsg}
+          className={
+            tweetMsg ? styles.tweet_sendBtn : styles.tweet_sendDisableBtn
+          }
+        >
+          Tweet
+        </Button>
+      </form>
+    </>
   );
 };
 
